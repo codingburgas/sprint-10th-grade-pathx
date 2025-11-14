c4590217515c46c51118bab9369b01a95caa0d93
KSTurmanov <KSTurmanov23@codingburgas.bg>
2025-11-14 17:43:08 +0200

    Update end screens

--- sprint-10th-grade-pathx/gameMedium.cpp
+++ sprint-10th-grade-pathx/gameMedium.cpp
@@ -14,7 +14,7 @@ struct Cell {
 };
 
 static Cell maze[GRID_H][GRID_W];
-static bool coins[GRID_H][GRID_W] = { false }; // Coin array
+static bool coins[GRID_H][GRID_W] = { false };
 static int coinsCollected = 0;
 static int totalCoins = 0;
 
@@ -25,55 +25,32 @@ static int endX = GRID_W - 1;
 static int endY = GRID_H - 1;
 
 static bool gameStarted = false;
-static bool reachedEnd = false;
 static float startTime = 0.0f;
 static float elapsedTime = 0.0f;
 
 static double blinkTimer = 0.0;
 static bool blinkLight = false;
 
-// Initialize coins in the maze
 static void InitializeCoins() {
     coinsCollected = 0;
     totalCoins = 0;
-
-    // Reset all coins
-    for (int y = 0; y < GRID_H; y++) {
-        for (int x = 0; x < GRID_W; x++) {
+    for (int y = 0; y < GRID_H; y++)
+        for (int x = 0; x < GRID_W; x++)
             coins[y][x] = false;
-        }
-    }
 
-    // Place coins randomly (about 15% of cells)
     for (int y = 0; y < GRID_H; y++) {
         for (int x = 0; x < GRID_W; x++) {
-            // Don't place coins at start or end positions
-            if ((x == 0 && y == 0) || (x == endX && y == endY)) {
-                continue;
-            }
-
-            // Random chance to place coin (15% probability)
-            if (rand() % 100 < 15) {
-                coins[y][x] = true;
-                totalCoins++;
-            }
+            if ((x == 0 && y == 0) || (x == endX && y == endY)) continue;
+            if (rand() % 100 < 15) { coins[y][x] = true; totalCoins++; }
         }
     }
 }
 
 static void GenerateMazeDFS(int x, int y) {
-    maze[y][x].topWall = maze[y][x].topWall;
-    maze[y][x].bottomWall = maze[y][x].bottomWall;
-    maze[y][x].leftWall = maze[y][x].leftWall;
-    maze[y][x].rightWall = maze[y][x].rightWall;
-
     bool visited[GRID_H][GRID_W] = { false };
-    visited[y][x] = true;
-
     std::vector<Vector2> stack;
     stack.push_back({ (float)x, (float)y });
-
-    int dirs[4] = { 0, 1, 2, 3 };
+    visited[y][x] = true;
 
     while (!stack.empty()) {
         Vector2 cell = stack.back();
@@ -81,7 +58,6 @@ static void GenerateMazeDFS(int x, int y) {
         int cy = (int)cell.y;
 
         std::vector<int> neighbors;
-
         if (cy > 0 && !visited[cy - 1][cx]) neighbors.push_back(0); // top
         if (cx < GRID_W - 1 && !visited[cy][cx + 1]) neighbors.push_back(1); // right
         if (cy < GRID_H - 1 && !visited[cy + 1][cx]) neighbors.push_back(2); // bottom
@@ -89,18 +65,14 @@ static void GenerateMazeDFS(int x, int y) {
 
         if (!neighbors.empty()) {
             int dir = neighbors[rand() % neighbors.size()];
-
             if (dir == 0) { maze[cy][cx].topWall = false; maze[cy - 1][cx].bottomWall = false; cy--; }
             else if (dir == 1) { maze[cy][cx].rightWall = false; maze[cy][cx + 1].leftWall = false; cx++; }
             else if (dir == 2) { maze[cy][cx].bottomWall = false; maze[cy + 1][cx].topWall = false; cy++; }
             else if (dir == 3) { maze[cy][cx].leftWall = false; maze[cy][cx - 1].rightWall = false; cx--; }
-
             stack.push_back({ (float)cx, (float)cy });
             visited[cy][cx] = true;
         }
-        else {
-            stack.pop_back();
-        }
+        else { stack.pop_back(); }
     }
 }
 
@@ -114,25 +86,15 @@ static void DrawMazeLines(int cell, int ox, int oy) {
             if (maze[y][x].rightWall) DrawLine(sx + cell, sy, sx + cell, sy + cell, WHITE);
             if (maze[y][x].bottomWall) DrawLine(sx, sy + cell, sx + cell, sy + cell, WHITE);
 
-            // Draw coin if it exists and hasn't been collected
-            if (coins[y][x]) {
-                DrawCircle(sx + cell / 2, sy + cell / 2, cell / 6, YELLOW);
-            }
+            if (coins[y][x]) DrawCircle(sx + cell / 2, sy + cell / 2, cell / 6, YELLOW);
         }
     }
-
     DrawRectangle(ox + endX * cell + 2, oy + endY * cell + 2, cell - 4, cell - 4, GREEN);
 }
 
-// Separate function to draw coin counter
 static void DrawCoinCounter() {
-    // Background for coin counter
     DrawRectangle(440, 45, 250, 40, Fade(BLACK, 0.7f));
-
-    // Coin icon
     DrawCircle(450, 65, 12, YELLOW);
-
-    // Coin counter text
     DrawText("COINS:", 470, 50, 30, WHITE);
     DrawText(TextFormat("%d/%d", coinsCollected, totalCoins), 600, 50, 30, YELLOW);
 }
@@ -140,7 +102,7 @@ static void DrawCoinCounter() {
 void StartMediumGame() {
     srand((unsigned int)time(0));
     GenerateMazeDFS(0, 0);
-    InitializeCoins(); // Initialize coins after generating maze
+    InitializeCoins();
 
     const int screenWidth = 720;
     const int screenHeight = 720;
@@ -148,93 +110,72 @@ void StartMediumGame() {
     InitWindow(screenWidth, screenHeight, "Maze - Medium");
     SetTargetFPS(60);
 
-    int cell = (screenWidth / GRID_W < screenHeight / GRID_H)
-        ? screenWidth / GRID_W
-        : screenHeight / GRID_H;
-    if (cell < 4) cell = 4;
-
+    int cell = (screenWidth / GRID_W < screenHeight / GRID_H) ? screenWidth / GRID_W : screenHeight / GRID_H;
     int offsetX = (screenWidth - cell * GRID_W) / 2;
     int offsetY = (screenHeight - cell * GRID_H) / 2;
 
-    playerX = 0;
-    playerY = 0;
+    playerX = 0; playerY = 0;
     gameStarted = false;
-    reachedEnd = false;
     startTime = 0.0f;
     elapsedTime = 0.0f;
 
+    enum GameState { PLAYING, WINSCREEN };
+    GameState state = PLAYING;
+
     while (!WindowShouldClose()) {
-
         blinkTimer += GetFrameTime();
-        if (!blinkLight && blinkTimer >= 4.5) {
-            blinkTimer = 0.0;
-            blinkLight = true;
-        }
-        else if (blinkLight && blinkTimer >= 2.0) {
-            blinkTimer = 0.0;
-            blinkLight = false;
-        }
+        if (!blinkLight && blinkTimer >= 4.5) { blinkTimer = 0.0; blinkLight = true; }
+        else if (blinkLight && blinkTimer >= 123.0) { blinkTimer = 0.0; blinkLight = false; }
 
-        if (gameStarted) {
-            elapsedTime = (float)GetTime() - startTime;
-        }
+        if (gameStarted) elapsedTime = (float)GetTime() - startTime;
 
-        // Store old position for coin collection
-        int oldX = playerX;
-        int oldY = playerY;
+        if (state == PLAYING) {
+            int oldX = playerX, oldY = playerY;
+            if ((IsKeyPressed(KEY_W) || IsKeyPressed(KEY_UP)) && !maze[playerY][playerX].topWall) playerY--;
+            if ((IsKeyPressed(KEY_S) || IsKeyPressed(KEY_DOWN)) && !maze[playerY][playerX].bottomWall) playerY++;
+            if ((IsKeyPressed(KEY_A) || IsKeyPressed(KEY_LEFT)) && !maze[playerY][playerX].leftWall) playerX--;
+            if ((IsKeyPressed(KEY_D) || IsKeyPressed(KEY_RIGHT)) && !maze[playerY][playerX].rightWall) playerX++;
 
-        if ((IsKeyPressed(KEY_W) || IsKeyPressed(KEY_UP)) && !maze[playerY][playerX].topWall) playerY--;
-        if ((IsKeyPressed(KEY_S) || IsKeyPressed(KEY_DOWN)) && !maze[playerY][playerX].bottomWall) playerY++;
-        if ((IsKeyPressed(KEY_A) || IsKeyPressed(KEY_LEFT)) && !maze[playerY][playerX].leftWall) playerX--;
-        if ((IsKeyPressed(KEY_D) || IsKeyPressed(KEY_RIGHT)) && !maze[playerY][playerX].rightWall) playerX++;
+            if ((playerX != oldX || playerY != oldY) && coins[playerY][playerX]) { coins[playerY][playerX] = false; coinsCollected++; }
 
-        // Collect coin if player moved to a new cell with a coin
-        if ((playerX != oldX || playerY != oldY) && coins[playerY][playerX]) {
-            coins[playerY][playerX] = false;
-            coinsCollected++;
-        }
+            if (!gameStarted && (IsKeyPressed(KEY_W) || IsKeyPressed(KEY_A) || IsKeyPressed(KEY_S) || IsKeyPressed(KEY_D) ||
+                IsKeyPressed(KEY_UP) || IsKeyPressed(KEY_LEFT) || IsKeyPressed(KEY_DOWN) || IsKeyPressed(KEY_RIGHT))) {
+                gameStarted = true;
+                startTime = (float)GetTime();
+            }
 
-        if (!gameStarted && (IsKeyPressed(KEY_W) || IsKeyPressed(KEY_A) || IsKeyPressed(KEY_S) || IsKeyPressed(KEY_D) ||
-            IsKeyPressed(KEY_UP) || IsKeyPressed(KEY_LEFT) || IsKeyPressed(KEY_DOWN) || IsKeyPressed(KEY_RIGHT))) {
-            gameStarted = true;
-            startTime = (float)GetTime();
-        }
-
-        BeginDrawing();
-        ClearBackground(BLACK);
-
-        if (blinkLight) {
-            DrawMazeLines(cell, offsetX, offsetY);
-            DrawRectangle(offsetX + playerX * cell + 2, offsetY + playerY * cell + 2, cell - 4, cell - 4, YELLOW);
-
+            BeginDrawing();
+            ClearBackground(BLACK);
+            if (blinkLight) {
+                DrawMazeLines(cell, offsetX, offsetY);
+                DrawRectangle(offsetX + playerX * cell + 2, offsetY + playerY * cell + 2, cell - 4, cell - 4, YELLOW);
+            }
             int totalSeconds = (int)elapsedTime;
             int minutes = totalSeconds / 60;
             int seconds = totalSeconds % 60;
-
-            // Draw time counter
             DrawText("TIME:", 440, 0, 40, WHITE);
             DrawText(TextFormat("%02d:%02d", minutes, seconds), 560, 0, 40, WHITE);
-
-            // Draw coin counter ONLY when maze is visible
             DrawCoinCounter();
+            EndDrawing();
+
+            if (playerX == endX && playerY == endY) state = WINSCREEN;
         }
-        // Always show timer and coin counter, even when screen is black
-        else {
+        else if (state == WINSCREEN) {
+            BeginDrawing();
+            ClearBackground(BLACK);
+            DrawText("YOU WIN!", screenWidth / 2 - 150, screenHeight / 2 - 120, 60, GOLD);
             int totalSeconds = (int)elapsedTime;
             int minutes = totalSeconds / 60;
             int seconds = totalSeconds % 60;
+            DrawText(TextFormat("Time: %02d:%02d", minutes, seconds), screenWidth / 2 - 100, screenHeight / 2, 40, WHITE);
+            DrawText(TextFormat("Coins: %d/%d", coinsCollected, totalCoins), screenWidth / 2 - 100, screenHeight / 2 + 50, 40, YELLOW);
+            DrawText("Press R to Play Again", screenWidth / 2 - 160, screenHeight / 2 + 120, 30, RAYWHITE);
+            DrawText("Press ESC for Menu", screenWidth / 2 - 140, screenHeight / 2 + 160, 30, RAYWHITE);
+            EndDrawing();
 
-            // Draw time counter
-            DrawText("TIME:", 440, 0, 40, WHITE);
-            DrawText(TextFormat("%02d:%02d", minutes, seconds), 560, 0, 40, WHITE);
-
-            // Draw coin counter
-            DrawCoinCounter();
+            if (IsKeyPressed(KEY_R)) { CloseWindow(); StartMediumGame(); return; }
+            if (IsKeyPressed(KEY_ESCAPE)) break;
         }
-
-        EndDrawing();
-
-        if (playerX == endX && playerY == endY) break;
     }
 
     CloseWindow();
