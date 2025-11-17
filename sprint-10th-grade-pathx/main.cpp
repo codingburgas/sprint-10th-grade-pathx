7f2bbab9f8274c167b915f48563c0523ac580ee2
lzkumanov23 <lzkumanov23@codingburgas.bg>
2025-11-17 23:18:06 +0200

    Add levelling system

--- sprint-10th-grade-pathx/main.cpp
+++ sprint-10th-grade-pathx/main.cpp
@@ -8,8 +8,18 @@
 
 enum GameState { STATE_MAIN_MENU, STATE_DIFFICULTY, STATE_EXIT };
 
-// Global persistent coin counter
+// Global persistent coin counter and level system
 static int totalCoinsCollected = 0;
+static int playerLevel = 1;
+static int coinsForNextLevel = 10; // Coins needed to reach level 2
+
+// Function to calculate level based on total coins
+static void UpdatePlayerLevel() {
+    // Level formula: level increases every 10 coins
+    // Level 1: 0-9 coins, Level 2: 10-19 coins, Level 3: 20-29 coins, etc.
+    playerLevel = (totalCoinsCollected / 10) + 1;
+    coinsForNextLevel = playerLevel * 10; // Next level requires current level * 10 coins
+}
 
 int main() {
     int monitor = GetCurrentMonitor();
@@ -26,7 +36,9 @@ int main() {
 
     InitAudioDevice();
     Sound clickSound = LoadSound("click.wav");
+    Sound levelUpSound = LoadSound("levelup.wav"); // You'll need to add this sound file
     SetSoundVolume(clickSound, 1.0f);
+    SetSoundVolume(levelUpSound, 1.0f);
 
     GameState state = STATE_MAIN_MENU;
 
@@ -59,6 +71,7 @@ int main() {
         switch (state) {
 
         case STATE_MAIN_MENU:
+        {
             if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
                 if (CheckCollisionPointRec(mouse, btnHelp)) { PlaySound(clickSound); ShowHelp(); }
                 if (CheckCollisionPointRec(mouse, btnExit)) { PlaySound(clickSound); state = STATE_EXIT; }
@@ -71,9 +84,26 @@ int main() {
             // Title
             DrawTextEx(uiFont, "PATH X", { float(SW / 2 - 240), 40 }, 96, 4, GOLD);
 
+            // Level display
+            DrawTextEx(uiFont, TextFormat("LEVEL: %d", playerLevel), { float(SW / 2 - 100), 130 }, 50, 3, BLUE);
+
+            // Progress bar background
+            DrawRectangle(SW / 2 - 150, 190, 300, 30, DARKGRAY);
+
+            // Calculate progress bar
+            int currentLevelCoins = totalCoinsCollected % 10;
+            float progress = (float)currentLevelCoins / 10.0f;
+            int progressWidth = (int)(300 * progress);
+
+            // Progress bar fill
+            DrawRectangle(SW / 2 - 150, 190, progressWidth, 30, GREEN);
+
+            // Progress text
+            DrawTextEx(uiFont, TextFormat("%d/10", currentLevelCoins), { float(SW / 2 - 30), 192 }, 24, 2, WHITE);
+
             // Total coins
             DrawTextEx(uiFont, TextFormat("TOTAL COINS COLLECTED: %d", totalCoinsCollected),
-                { float(SW / 2 - 250), 150 }, 40, 2, YELLOW);
+                { float(SW / 2 - 250), 230 }, 36, 2, YELLOW);
 
             // Buttons
             DrawRectangleRec(btnPlay, DARKGRAY);
@@ -87,14 +117,44 @@ int main() {
 
             EndDrawing();
             break;
+        }
 
         case STATE_DIFFICULTY:
+        {
             if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-                if (CheckCollisionPointRec(mouse, difficultyButtons[0])) { PlaySound(clickSound); StartEasyGame();   totalCoinsCollected += GetEasyGameCoins(); }
-                if (CheckCollisionPointRec(mouse, difficultyButtons[1])) { PlaySound(clickSound); StartMediumGame(); totalCoinsCollected += GetMediumGameCoins(); }
-                if (CheckCollisionPointRec(mouse, difficultyButtons[2])) { PlaySound(clickSound); StartHardGame();   totalCoinsCollected += GetHardGameCoins(); }
-                if (CheckCollisionPointRec(mouse, difficultyButtons[3])) { PlaySound(clickSound); StartGameDoorMaze(); totalCoinsCollected += GetDoorMazeCoins(); }
-                if (CheckCollisionPointRec(mouse, difficultyButtons[4])) { PlaySound(clickSound); StartGameSecMaze(); totalCoinsCollected += GetSecMazeCoins(); }
+                int oldLevel = playerLevel;
+
+                if (CheckCollisionPointRec(mouse, difficultyButtons[0])) {
+                    PlaySound(clickSound);
+                    StartEasyGame();
+                    totalCoinsCollected += GetEasyGameCoins();
+                }
+                if (CheckCollisionPointRec(mouse, difficultyButtons[1])) {
+                    PlaySound(clickSound);
+                    StartMediumGame();
+                    totalCoinsCollected += GetMediumGameCoins();
+                }
+                if (CheckCollisionPointRec(mouse, difficultyButtons[2])) {
+                    PlaySound(clickSound);
+                    StartHardGame();
+                    totalCoinsCollected += GetHardGameCoins();
+                }
+                if (CheckCollisionPointRec(mouse, difficultyButtons[3])) {
+                    PlaySound(clickSound);
+                    StartGameDoorMaze();
+                    totalCoinsCollected += GetDoorMazeCoins();
+                }
+                if (CheckCollisionPointRec(mouse, difficultyButtons[4])) {
+                    PlaySound(clickSound);
+                    StartGameSecMaze();
+                    totalCoinsCollected += GetSecMazeCoins();
+                }
+
+                // Update level and check for level up
+                UpdatePlayerLevel();
+                if (playerLevel > oldLevel) {
+                    PlaySound(levelUpSound);
+                }
             }
 
             if (IsKeyPressed(KEY_ESCAPE)) state = STATE_MAIN_MENU;
@@ -104,7 +164,14 @@ int main() {
 
             DrawTextEx(uiFont, "PATH X", { float(SW / 2 - 240), 40 }, 96, 4, GOLD);
             DrawTextEx(uiFont, "CHOOSE GAME MODE", { float(SW / 2 - 330), 150 }, 60, 3, WHITE);
-            DrawTextEx(uiFont, TextFormat("TOTAL COINS: %d", totalCoinsCollected), { float(SW / 2 - 150), 220 }, 36, 2, YELLOW);
+
+            // Level and progress in difficulty screen
+            DrawTextEx(uiFont, TextFormat("LEVEL: %d", playerLevel), { float(SW / 2 - 100), 220 }, 40, 3, BLUE);
+            DrawTextEx(uiFont, TextFormat("TOTAL COINS: %d", totalCoinsCollected), { float(SW / 2 - 150), 270 }, 36, 2, YELLOW);
+
+            // Progress to next level
+            int currentLevelCoinsDiff = totalCoinsCollected % 10;
+            DrawTextEx(uiFont, TextFormat("NEXT LEVEL: %d/10", currentLevelCoinsDiff), { float(SW / 2 - 150), 310 }, 28, 2, GREEN);
 
             for (int i = 0; i < btnCount; i++) {
                 DrawRectangleRec(difficultyButtons[i], GRAY);
@@ -115,9 +182,11 @@ int main() {
 
             EndDrawing();
             break;
+        }
 
         case STATE_EXIT:
             UnloadSound(clickSound);
+            UnloadSound(levelUpSound);
             UnloadFont(uiFont);
             CloseAudioDevice();
             CloseWindow();
@@ -126,6 +195,7 @@ int main() {
     }
 
     UnloadSound(clickSound);
+    UnloadSound(levelUpSound);
     UnloadFont(uiFont);
     CloseAudioDevice();
     CloseWindow();
